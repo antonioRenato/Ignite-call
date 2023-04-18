0b55c8344981fa950fe62dce6b8e1ca5788737ff
antonioRenato <antoniorpfs@gmail.com>
2023-04-17 21:56:46 -0300

    add page home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading>Agendamento descomplicado</Heading>
+        <Text></Text>
+      </Hero>
+    </Container>
+  )
+}
