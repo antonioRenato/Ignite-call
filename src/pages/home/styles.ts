7b681e9b24ed787287069c77463cf66927d30d61
antonioRenato <antoniorpfs@gmail.com>
2023-04-18 19:45:37 -0300

    created front home

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,36 @@
-import { styled } from '@ignite-ui/react'
+import { Heading, Text, styled } from '@ignite-ui/react'
 
-export const Container = styled('div', {})
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) /2)',
+  marginLeft: 'auto',
+  height: '100vh',
 
-export const Hero = styled('div', {})
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+})
 
-export const Preview = styled('div', {})
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`> ${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$8',
+    },
+  },
+
+  [`${Text}`]: {
+    marginTop: '$2',
+    color: '$gray100',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
