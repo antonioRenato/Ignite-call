f9151d8c7b7d8b49c697225fc5ebd04c4d4c4c1b
antonioRenato <antoniorpfs@gmail.com>
2023-04-27 21:36:51 -0300

    create time intervals

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,19 @@
+import { Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+// import { api } from '@/lib/axios'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+    </Container>
+  )
+}
