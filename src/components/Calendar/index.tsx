15efce965bda464d826b86f7d8144666853feefc
antonioRenato <antoniorpfs@gmail.com>
2023-05-05 04:36:10 -0300

    component calendar index done

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,12 +1,17 @@
 import { CaretLeft, CaretRight } from 'phosphor-react'
 import {
   CalendarActions,
+  CalendarBody,
   CalendarContainer,
+  CalendarDay,
   CalendarHeader,
   CalendarTitle,
 } from './styles'
+import { getWeekDays } from '@/utils/get-week-days'
 
 export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+
   return (
     <CalendarContainer>
       <CalendarHeader>
@@ -22,6 +27,33 @@ export function Calendar() {
           </button>
         </CalendarActions>
       </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
     </CalendarContainer>
   )
 }
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,5 +1,10 @@
+import { Calendar } from '@/components/Calendar'
 import { Container } from './styles'
 
 export function CalendarStep() {
-  return <Container />
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
 }
