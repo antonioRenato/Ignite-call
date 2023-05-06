a2f49861e5c8aefd030de7850bef8d370309fecc
antonioRenato <antoniorpfs@gmail.com>
2023-05-06 18:38:31 -0300

    finished Calendar step page

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,5 +1,11 @@
 import { Calendar } from '@/components/Calendar'
-import { Container, TimePicker } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
   const isDateSelected = true
@@ -8,7 +14,26 @@ export function CalendarStep() {
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
 
-      {isDateSelected && <TimePicker />}
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça feira <span>05 de maio</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
